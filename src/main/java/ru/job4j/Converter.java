8409946fb7ac7bc75a407d8a02b3c2608b766508
pokahFish <pokahfish@gmail.com>
2022-11-22 11:29:01 +0400

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -1,4 +1,23 @@
 package ru.job4j;
 
+import java.util.SplittableRandom;
+
 public class Converter {
+
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return  rsl;
+    }
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 60;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        float dollars = Converter.rubleToDollar(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("140 rubles are " + dollars + " dollars.");
+    }
 }
