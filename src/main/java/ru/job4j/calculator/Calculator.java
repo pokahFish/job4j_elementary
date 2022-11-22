5cb2f145c329e44e9c9da9801504430cad2d82fa
pokahFish <pokahfish@gmail.com>
2022-11-22 11:18:55 +0400

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,15 @@
-package ru.job4j;
+package ru.job4j.calculator;
 
 public class Calculator {
 
     public static void plus(int first, int second) {
-
         int result = first + second;
         System.out.println(result);
     }
 
     public static void main(String[] args) {
-
-        Calculator.plus(100, 500);
-        Calculator.plus(4, 2);
-        Calculator.plus(3, 5);
-
+        Calculator.plus(1, 2);
+        Calculator.plus(10, 11);
     }
 }
 
---
+++ src/main/java/ru/job4j/calculator/Multiple.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculator;
+
+public class Multiple {
+    public static void main(String[] args) {
+
+        int ageHuman = 36;
+        char genderMale = 'M';
+        double heightHuman = 1.75;
+        int brick = 123445;
+        double bacterialCount = 9.84E+6;
+        double solarDistance = 1.4779E+8;
+        boolean workAuth = true;
+        int fileSize = 64999483;
+
+    }
+}
