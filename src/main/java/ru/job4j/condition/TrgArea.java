34c9b42d299f1d56fba0a651cf7441f8618d057c
pokahFish <pokahfish@gmail.com>
2022-11-24 13:12:01 +0400

    9. Площадь треугольника. [#175474 #363803]

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -3,9 +3,8 @@ import java.lang.Math;
 
 public class TrgArea {
     public static double area(double a, double b, double c) {
-        double semiPer = (a + b + c) / 2;
-        System.out.println(semiPer);
-        double rsl = Math.sqrt(semiPer * ((semiPer - a) * (semiPer - b) * (semiPer - c)));
+        double p = (a + b + c) / 2;
+        double rsl = Math.sqrt(p * ((p - a) * (p - b) * (p - c)));
         return rsl;
     }
 
