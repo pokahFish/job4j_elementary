82d2c820d25c77f5b2cf3417e446ae210f4bfa0e
pokahFish <pokahfish@gmail.com>
2022-11-26 16:15:48 +0400

    1.2. Входные данные в тесте [#297750]

---
+++ src/main/java/ru/job4j/ForFun.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+import java.util.Scanner;
+
+public class ForFun {
+    public static void main(String[] args) {
+        short s = 1500;
+        char c = (char) s;
+        double d = (double) c;
+        System.out.println(d);
+
+    }
+}
---
+++ src/test/java/ru/job4j/math/MathFunctionTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathFunctionTest {
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void sum() {
+    }
+
+    @Test
+    void minus() {
+    }
+
+    @Test
+    void divide() {
+    }
+}
