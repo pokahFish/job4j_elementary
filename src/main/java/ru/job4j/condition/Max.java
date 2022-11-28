cf817ed5f54056a6706a71e5cfb50c0cfe2cd3e0
pokahFish <pokahfish@gmail.com>
2022-11-28 16:17:06 +0400

    4.2. Тернарное сравнение [#189 #364761]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,9 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean conditional = left > right;
-        int result = conditional ? left : right;
-        return result;
+        return left > right ? left : right;
     }
 
     public static void main(String[] args) {
