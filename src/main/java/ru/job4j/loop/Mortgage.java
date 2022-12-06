a0949bb76511779083104a07fc1f132e71b2745c
pokahFish <pokahfish@gmail.com>
2022-12-06 18:21:03 +0400

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -1,7 +1,6 @@
 package ru.job4j.loop;
 
 public class Mortgage {
-
         public static int year(int amount, int salary, double percent) {
             int year = 0;
             double truPercent = percent / 100;
