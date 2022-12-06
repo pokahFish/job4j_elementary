5d44c0f6238e76a8b925802596ac556cee962888
pokahFish <pokahfish@gmail.com>
2022-12-06 18:18:52 +0400

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+        public static int year(int amount, int salary, double percent) {
+            int year = 0;
+            double truPercent = percent / 100;
+            double debt = amount * truPercent + amount;
+
+            while (debt > 0) {
+                debt = debt - salary;
+                year++;
+           }
+            return year;
+        }
+}
