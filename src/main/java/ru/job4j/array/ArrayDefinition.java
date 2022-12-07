95d9f2191c9d35d44f428da767de56b8d3b0a57e
pokahFish <pokahfish@gmail.com>
2022-12-07 17:33:43 +0400

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Oleg Ivanov";
+        System.out.println(names[0]);
+        names[1] = "Alex Gir";
+        System.out.println(names[1]);
+        names[2] = "Susan Gold";
+        System.out.println(names[2]);
+        names[3] = "Bob G";
+        System.out.println(names[3]);
     }
 }
