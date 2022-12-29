a715b78c0ca8d083725a2e1ee60a095e2a049e6f
pokahFish <pokahfish@gmail.com>
2022-12-29 09:24:02 +0400

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/SomeTest.java
@@ -0,0 +1,8 @@
+package ru.job4j.array;
+
+public class SomeTest {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2}, {3, 4}};
+        System.out.println(array[1][0]);
+            }
+}
--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,26 +1,21 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-
     public static void plus(int first, int second) {
         int result = first + second;
         System.out.println(result);
     }
 
-    public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int six = 6;
-        int five = 5;
-        int four = 4;
-        int onePlusTwo = one + two;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+    public static void minus(int first, int second) {
+        int result = first - second;
+        System.out.println(result);
+    }
 
+    public static void main(String[] args) {
+        plus(1, 2);
+        plus(123, 7);
+        minus(10, 0);
+        minus(60, 10);
     }
 }
 
